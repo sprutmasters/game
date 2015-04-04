7071dddaa7d163257b16b80d2ff75c47c10d4f4a
Sejkhaa <Sejkhaa@gmail.com>
2015-04-04 23:39:27 +0200

    stek

--- Falken/src/tilegame/main/entities/Entity.java
+++ Falken/src/tilegame/main/entities/Entity.java
@@ -17,4 +17,39 @@ public abstract class Entity {
 	public abstract void tick();
 	
 	public abstract void render(Graphics g);
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	
 }
+
