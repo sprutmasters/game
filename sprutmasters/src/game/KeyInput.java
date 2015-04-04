526ba3f18ec987d6a3bb5e4864a3611c796fca63
drontFAN <viktor.johansson1991@live.se>
2015-04-04 02:33:27 +0200

    asd
    asd

---
+++ sprutmasters/src/game/KeyInput.java
@@ -0,0 +1,16 @@
+package game;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyInput extends KeyAdapter {
+	
+	public void keyPressed(KeyEvent e){
+		int key = e.getKeyCode();
+	}
+	
+	public void keyReleased(KeyEvent e){
+		
+	}
+	
+}
