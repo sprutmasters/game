ad490b2d5f1f6ffa224f257777597af98de2579e
Sejkhaa <Sejkhaa@gmail.com>
2015-04-04 15:27:45 +0200

    New Class "Spritelist"
    Added "Spritelist" for tiles1.png for future reference.

---
+++ Falken/src/tilegame/gfx/Spritelist.java
@@ -0,0 +1,54 @@
+
+
+//package tilegame.gfx;
+
+//public class Spritelist {
+
+	
+	
+	
+	//FROM TOP LEFT CORNER TO RIGHT IN TILES1.PNG FILE!!
+
+
+	//GRASS
+	//g.drawImage(sheet.crop(0, 0, 32, 32), 5, 5, null);
+	
+	//GRAVEL1
+	//g.drawImage(sheet.crop(32, 0, 32, 32), 5, 5, null);
+
+	//GRAVEL2
+	//g.drawImage(sheet.crop(64, 0, 32, 32), 5, 5, null);
+
+	//GRAVEL3
+	//g.drawImage(sheet.crop(96, 0, 32, 32), 5, 5, null);
+
+	//ASPHALT DARK
+	//g.drawImage(sheet.crop(128, 0, 32, 32), 5, 5, null);
+
+	//ASPHALT
+	//g.drawImage(sheet.crop(140, 0, 32, 32), 5, 5, null);
+
+	//DIRT
+	//g.drawImage(sheet.crop(160, 0, 32, 32), 5, 5, null);
+
+	//GRASS 2
+	//g.drawImage(sheet.crop(192, 0, 32, 32), 5, 5, null);
+
+	//WOOD LIGHT
+	//g.drawImage(sheet.crop(224, 0, 32, 32), 5, 5, null);
+
+	//WOOD TRUNK
+	//g.drawImage(sheet.crop(256, 0, 32, 32), 5, 5, null);
+
+	//WOOD DARK
+	//g.drawImage(sheet.crop(288, 0, 32, 32), 5, 5, null);
+
+	
+
+	
+	
+	
+	
+	
+	
+//}
