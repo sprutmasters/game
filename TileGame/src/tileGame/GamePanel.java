9096ea277a2e76c213d3e9cbd0eccf9a3f9d9c91
drontFAN <viktor.johansson1991@live.se>
2015-04-06 11:42:55 +0200

    fixat wait MEN
    vi har nå problem med att mappar ska finnas men finns inte

--- TileGame/src/tileGame/GamePanel.java
+++ TileGame/src/tileGame/GamePanel.java
@@ -71,6 +71,9 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
 			elapsed = System.nanoTime() - start;
 			
 			wait = targetTime - elapsed / 1000000;
+			if(wait < 0){
+				wait = 5;
+			}
 			
 			try{
 				Thread.sleep(wait);
